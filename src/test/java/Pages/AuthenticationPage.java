c069a1734b33954dcf8cceed6421b8f2cb3a88b1
Roberto Ferraz <roberto_ferraz15@hotmail.com>
2018-07-04 22:51:05 -0300

    New Commit

--- src/test/java/Pages/AuthenticationPage.java
+++ src/test/java/Pages/AuthenticationPage.java
@@ -2,6 +2,8 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AuthenticationPage extends BasePage{
 
@@ -12,6 +14,10 @@ public class AuthenticationPage extends BasePage{
     public createAnAccountPage typeEmail(String email_create){
         driver.findElement(By.id("email_create")).sendKeys(email_create);
         driver.findElement(By.xpath("//*[@id=\"SubmitCreate\"]/span")).click();
+
+        //WebDriverWait waiting = new WebDriverWait(driver, 30);
+        //waiting.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+               // "//*[@id=\\\"content\\\"]/section[2]/div/div/div/div[4]/a/div/div/h3"))).click();
 
         return new createAnAccountPage(driver);
     }
